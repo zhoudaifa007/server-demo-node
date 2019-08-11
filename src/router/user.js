d19d6c663fe7a6cc0e3603f0f9a94cc7f79edc35
yhan <1341282966@qq.com>
2019-08-12 07:57:52 +0800

    添加分页查询

--- src/router/user.js
+++ src/router/user.js
@@ -1,18 +1,27 @@
 const mysqlOperation = require('../utils/connectDataBase');
 const PageIfo = require('../model/PageInfo');
 
-const query = function (id, operation) {
+const queryById = function (id, operation, currentPage = 0, pageSize = 10) {
   const pool = mysqlOperation.createPool();
   // mysqlOperation.query(operation, connect, 'name', 'user');
-  const pageInfo = new PageIfo();
+  const pageInfo = new PageIfo(currentPage, pageSize);
   mysqlOperation.query(operation, pool, 'name', 'user', pageInfo, 'id = ' + id);
+}
+
+const query = function (operation, currentPage = 0, pageSize = 10) {
+  const pool = mysqlOperation.createPool();
+  // mysqlOperation.query(operation, connect, 'name', 'user');
+  const pageInfo = new PageIfo(currentPage, pageSize);
+  mysqlOperation.query(operation, pool, '*', 'user', pageInfo);
 }
 
 function show(result) {
   // console.log(result);
-  let send = '';
-  if (result.length > 0) {
+  let send = null;
+  if (result.length == 1) {
     send = result[0].name
+  } else if (result.length > 1) {
+    send = result;
   } else {
     send = '没有查到该用户';
   }
@@ -20,11 +29,16 @@ function show(result) {
 }
 
 const router = function (app) {
-  app.get('/user', function (req, res) {
-    const userId = req.query.id;
-    query(userId, show.bind({
-      res
-    }));
+  app.get('/api/user', function (req, res) {
+    if ('id' in req.query) {
+      queryById(req.query.id, show.bind({
+        res
+      }));
+    } else {
+      query(show.bind({
+        res
+      }));
+    }
   })
 }
 
